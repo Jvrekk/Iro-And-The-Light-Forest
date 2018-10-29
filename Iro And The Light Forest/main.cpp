1eb5e203e391e459279fa3af169a92d2db2f1542
Jvrekk <you@example.com>
2018-10-29 12:38:45 +0100

    changed to SFML, simple shape, test OK

--- Iro And The Light Forest/main.cpp
+++ Iro And The Light Forest/main.cpp
@@ -1,77 +1,31 @@
-#include <windows.h>	// standard Windows app include
-#include <string>
-#include <iostream>
-#include <time.h>
-#include <math.h>
-//OpenGL
-#include <GLFW/glfw3.h>
-#include <gl/gl.h>	// standard OpenGL include
-#include <gl/glu.h>	// OpenGL utilties
-#include <GL/glut.h>
-
-using namespace std;
+#include <SFML/Graphics.hpp>
+using namespace sf;
 
 
-
-//TESTOWE KOLKO JAPIERDOLE TU SIE RYSUJE PRAWIE ZE PISKELAMNI
-void DrawCircle(float cx, float cy, float r, int num_segments)
+int main()
 {
 
-	int i;
-	float theta = 2 * 3.1415926 / num_segments;
-	float c = cosf(theta);
-	float s = sinf(theta);
-	float t;
-	float x = r;
-	float y = 0;
-	glBegin(GL_LINE_LOOP);
-	for (i = 0; i < num_segments; i++)
+	RenderWindow window(sf::VideoMode(800, 600), "SFML works!");
+
+
+
+	CircleShape shape(150);
+	shape.setPosition(100, 100);
+	shape.setFillColor(sf::Color(100, 250, 50));
+
+	while (window.isOpen())
 	{
+		Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == Event::Closed)
+				window.close();
+		}
 
-		glVertex2f(x + cx, y + cy);
-		t = x;
-		x = c * x - s * y;
-		y = s * t + c * y;
+		window.clear();
+		window.draw(shape);
+		window.display();
 	}
-	glEnd();
-}
-// TAKI PRAWIE ZE GAME LOOP 
-void Display(void)
-{
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();                                  // Reset The Current Matrix to E
-	glOrtho(0, 800, 600, 0, 0, 1);
-	glMatrixMode(GL_MODELVIEW);
 
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);  // Clear The Screen And The Depth Buffer
-	glLoadIdentity();  // Reset The View
-
-	DrawCircle(200, 200, 50, 360); //Draw circle	
-	DrawCircle(100, 100, 50, 360); //check
-	
-	//TRIANGLE
-	glBegin(GL_TRIANGLES);
-
-	glColor3f(1, 1, 1);
-	glVertex2f(300.0, 210.0);
-	glVertex2f(340.0, 215.0);
-	glVertex2f(320.0, 250.0);
-
-	glEnd();
-
-	glFlush();
-
-}
-
-
-int main(int argc, char **argv)
-{
-	srand(time(NULL));
-	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
-	glutInitWindowSize(800, 600);
-	glutCreateWindow("Intro");
-	glutDisplayFunc(Display); 
-	glutMainLoop();
 	return 0;
 }
